4cdee89f1a63d3d37e2117f597c72a8834be9e99
Michał Jarząbek <nebraska994@gmail.com>
2017-06-10 10:24:43 +0200

    typ liczbowy i typ łańcuchowy

--- 3-typydanych.js
+++ 3-typydanych.js
@@ -1,2 +1,32 @@
 'use strict'
 
+/*
+
+Typ Liczbowy
+
+*/
+
+var wyplata = 5000;
+var premia = 2500;
+var calkowitaWyplata;
+
+calkowitaWyplata = wyplata + premia;
+
+console.log(calkowitaWyplata);
+
+
+/*Typ - łańcuch znaków-string*/
+
+
+var wyplataStr = "5000";
+var premiaStr = "2500";
+var calkowitaWyplataStr;
+
+
+calkowitaWyplataStr = wyplataStr + premiaStr;
+
+console.log(calkowitaWyplataStr);
+
+/*przy dodawaniu typów gdu jeden element będzie stringiem, to js zmieni  wszystkie inne na stringi*/
+
+/* przy dodawaniu typu liczbowe się sumują , a typy string wyświetla obok siebie bez spacji, doda nie liczbowe a ciąg znaków*/
