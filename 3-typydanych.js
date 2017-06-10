10451b3b0083841c3f5a43e99f8293177bcce703
Michał Jarząbek <nebraska994@gmail.com>
2017-06-10 10:50:56 +0200

    boolen

--- 3-typydanych.js
+++ 3-typydanych.js
@@ -23,10 +23,29 @@ var premiaStr = "2500";
 var calkowitaWyplataStr;
 
 
-calkowitaWyplataStr = wyplataStr + premiaStr;
+calkowitaWyplataStr = wyplataStr + " \"dodajemy\"  "+ premiaStr;
 
 console.log(calkowitaWyplataStr);
 
 /*przy dodawaniu typów gdu jeden element będzie stringiem, to js zmieni  wszystkie inne na stringi*/
 
 /* przy dodawaniu typu liczbowe się sumują , a typy string wyświetla obok siebie bez spacji, doda nie liczbowe a ciąg znaków*/
+
+var imieStudenta = " Michał "
+
+console.log(imieStudenta.indexOf('c'));
+console.log(imieStudenta);
+console.log(imieStudenta.trim());
+
+/*komputer liczy od zera, czyli 0 =1*/
+
+/*Typ-Logiczny-boolen*/
+var czyJestSmog;
+czyJestSmog = false;
+
+if (czyJestSmog){
+	console.log("Jest SMOG");
+}
+else {
+	console.log("Nie ma SMOGa ;)")
+}
